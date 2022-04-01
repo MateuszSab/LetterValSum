7ccdec4e6f03686ff19dda458c5f1858684c1bab
m.sabatowski <ma.sabatowski@gmail.com>
2022-04-01 11:00:44 +0200

    name of a test fixed

--- src/test/scala/LetterValSumSpec.scala
+++ src/test/scala/LetterValSumSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.matchers.should
 
 class LetterValSumSpec extends AnyFlatSpec with should.Matchers {
 
-  "LetterValSumSpec" should "Given a string of lowercase letters, " +
+  "LetterValSum" should "Given a string of lowercase letters, " +
                               "find the sum of the values of the letters in the string." in {
     import LetterValSum._
     sumLetters("abc") shouldBe 6
